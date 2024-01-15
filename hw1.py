d73c51b4d3bc80e41913a4e0aaaa5022b9b07fef
ty-runner <ty.runner02@gmail.com>
2024-01-14 19:06:58 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,6 +1,15 @@
 from BitVector import *
 
-def cryptBreak(encrypted_file, key_bv):
+def key_from_passphrase(passphrase, key_size):
+    # Generate a key using the passphrase
+    key = BitVector(textstring=passphrase)
+    while key.length() < key_size:
+        key += key  # Repeat the key until it reaches the desired length
+    return key
+
+def cryptBreak(encrypted_file, passphrase):
+    BLOCKSIZE = 16
+
     # Read the encrypted file
     with open(encrypted_file, 'r') as file:
         encrypted_text = file.read()
@@ -9,24 +18,24 @@ def cryptBreak(encrypted_file, key_bv):
     encrypted_bv = BitVector(hexstring=encrypted_text)
 
     # Brute-force attack to decrypt the message
-    for key in range(2**16):  # Keyspace size is 2^16 for a 16-bit key
-        key_bv = BitVector(intVal=key, size=16)
+    for key in range(2 ** BLOCKSIZE):  # Keyspace size is 2^BLOCKSIZE
+        key_bv = key_from_passphrase(passphrase, BLOCKSIZE)
         decrypted_bv = encrypted_bv ^ key_bv
 
         # Check if the decrypted message contains the word 'Ferrari'
         decrypted_message = decrypted_bv.get_text_from_bitvector()
-        print(key_bv, decrypted_message)
         if 'Ferrari' in decrypted_message:
-            return decrypted_message
+            return decrypted_message, key_bv
 
-    return "Decryption failed"
+    return "Decryption failed", None
 
 # Example usage
-RandomInteger = 9999  # Arbitrary integer for creating a BV
-key_bv = BitVector(intVal=RandomInteger, size=16)
-decryptedMessage = cryptBreak('cipherText.txt', key_bv)
+passphrase = "Hopes and dreams of a million years"
+decryptedMessage, key_bv = cryptBreak('cipherText.txt', passphrase)
 
 if 'Ferrari' in decryptedMessage:
     print('Encryption Broken!')
+    print('Decrypted Message:', decryptedMessage)
+    print('Encryption Key:', key_bv)
 else:
     print('Not decrypted yet')
