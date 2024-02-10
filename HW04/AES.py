49771dbb5351aea5e3a7669b4e8e03d56556a290
ty-runner <ty.runner02@gmail.com>
2024-02-10 10:44:05 -0500

    Update AES.py

--- HW04/AES.py
+++ HW04/AES.py
@@ -16,7 +16,7 @@ def gee(keyword, round_constant, byte_sub_table):
 class AES():
     def __init__(self, key_file:str) -> None:
         self.key = self.read_key(key_file)
-        self.key_schedule = self.key_schedule_128()
+        self.key_schedule = self.gen_key_schedule_256(self.key)
         self.genTables()
         self.encryption_sbox = subBytesTable
         self.decryption_sbox = invSubBytesTable
@@ -34,24 +34,25 @@ class AES():
             check = b.gf_MI(AES_modulus, 8)
             b = check if isinstance(check, BitVector) else 0
             invSubBytesTable.append(int(b))
-    def gen_key_schedule_256(key_bv:BitVector) -> list:
+    def gen_key_schedule_256(self, key_bv: BitVector) -> list:
         key_words = [None for i in range(60)]
         round_constant = BitVector(intVal=0x01, size=8)
         for i in range(8):
-            key_words[i] = key_bv[i*32 : i*32 + 32]
+            key_words[i] = key_bv[i * 32: i * 32 + 32]
         for i in range(8, 60):
-            if i%8 == 0:
-                kw, round_constant = gee(key_words[i-1], round_constant, subBytesTable)
-                key_words[i] = key_words[i-8] ^ kw
-            elif (i - (i//8)*8) < 4:
-                key_words[i] = key_words[i-8] ^ key_words[i-1]
-            elif (i - (i//8)*8) == 4:
+            if i % 8 == 0:
+                print(key_words)
+                kw, round_constant = gee(key_words[i - 1], round_constant, subBytesTable)
+                key_words[i] = key_words[i - 8] ^ kw
+            elif (i - (i // 8) * 8) < 4:
+                key_words[i] = key_words[i - 8] ^ key_words[i - 1]
+            elif (i - (i // 8) * 8) == 4:
                 key_words[i] = BitVector(size=0)
                 for j in range(4):
-                    key_words[i] += BitVector(intVal=subBytesTable[key_words[i-1][j*8:j*8+8].intValue()], size=8)
-                key_words[i] ^= key_words[i-8]
-            elif ((i - (i//8)*8) > 4) and ((i - (i//8)*8) < 8):
-                key_words[i] = key_words[i-8] ^ key_words[i-1]
+                    key_words[i] += BitVector(intVal=subBytesTable[key_words[i - 1][j * 8:j * 8 + 8].intValue()], size=8)
+                key_words[i] ^= key_words[i - 8]
+            elif ((i - (i // 8) * 8) > 4) and ((i - (i // 8) * 8) < 8):
+                key_words[i] = key_words[i - 8] ^ key_words[i - 1]
             else:
                 sys.exit("error in key scheduling algo for i = %d" % i)
         return key_words
@@ -60,20 +61,22 @@ class AES():
             key = file.read()
         return BitVector(textstring=key)
     
-    def encrypt(self, plaintext:str, ciphertext:str()) -> None:
+    def encrypt(self, plaintext:str, ciphertext:str) -> None:
         #each round of AES involves the following 4 steps:
         #1. Single-byte Substitution
+        print(subBytesTable)
+
         #2. Row-wise permutation
         #3. Column-wise mixing
         #4. Key addition
         bv = BitVector(filename=plaintext)
         FILEOUT = open(ciphertext, 'wb')
-        
+        #return bv.write_to_file(FILEOUT)
         pass
 
 
 if __name__ == "__main__":
-    cipher = AES(keyfile = sys.argv[3])
+    cipher = AES(key_file = sys.argv[3])
 
     if sys.argv[1] == "-e":
         cipher.encrypt(plaintext=sys.argv[2], ciphertext=sys.argv[4])
