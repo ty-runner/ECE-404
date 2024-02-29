bd736d3282524edf67f7958f8e85b619c5ee6e04
ty-runner <ty.runner02@gmail.com>
2024-02-29 15:29:20 -0500

    init

---
+++ HW07/sha512.py
@@ -0,0 +1,8 @@
+from BitVector import *
+
+# hash the ASCII in input.txt and write the resulting message digest to hashed.txt
+# write to hashed.txt as hexstring format
+
+
+
+#usability: python sha512.py input.txt hashed.txt
