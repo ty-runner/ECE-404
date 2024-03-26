798331648bcabc927b760a44c56131f519ba1342
ty-runner <ty.runner02@gmail.com>
2024-03-26 18:04:26 -0400

    Update firewall404.sh

--- HW09/firewall404.sh
+++ HW09/firewall404.sh
@@ -5,7 +5,8 @@ sudo iptables -F
 sudo iptables -X
 
 # Rule 1: Accept packets originating from f1.com
-sudo iptables -A INPUT -s f1.com -j ACCEPT
+f1="67.199.248.12"
+sudo iptables -A INPUT -s $f1 -j ACCEPT
 
 # Rule 2: Change outgoing packets' source IP address to your own machine's IP address (MASQUERADE)
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
@@ -27,7 +28,8 @@ sudo iptables -A OUTPUT -o lo -j ACCEPT
 sudo iptables -t nat -A PREROUTING -p tcp --dport 8888 -j DNAT --to-destination :25565
 
 # Rule 7: Allow outgoing SSH connections to engineering.purdue.edu
-sudo iptables -A OUTPUT -p tcp -d engineering.purdue.edu --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+engpurdue="128.46.104.20"
+sudo iptables -A OUTPUT -p tcp -d $engpurdue --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 # Rule 8: Drop any other packets
